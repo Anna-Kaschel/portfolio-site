4922d13d1369316d92b6d5fd2bb0f746a0410d5f
Akasche <anna.kaschel@gmail.com>
2021-03-17 17:46:33 -0400

    Correct navbar bleeding into media query, remove html for smooth-scroll behaviour

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
 console.log ("Hi! Welcome to My Portfolio Site")
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-}
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  x.className += ' responsive';
-}
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
